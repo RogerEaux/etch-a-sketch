28cd4349fe4daba660144012cfc1678cfc9dce5b
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-22 18:30:11 -0600

    Add css rules to display grid

--- js/script.js
+++ js/script.js
@@ -2,12 +2,15 @@ function createGrid(sizeOfGrid){
     const grid = document.querySelector('#grid');
     for(let i = 0; i < sizeOfGrid; i++){
         let row = document.createElement('div');
+        row.setAttribute('class','container row');
         for(let j = 0; j < sizeOfGrid; j++){
             let column = document.createElement('div');
+            column.classList.add('column')
+            column.textContent = 'C';
             row.appendChild(column);
         }
         grid.appendChild(row);   
     }
 }
 
-createGrid(2);
+createGrid(16);
