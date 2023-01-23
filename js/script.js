c1e2e373046ba7ca11789c17e07bd2b2998aba7b
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-22 18:56:19 -0600

    Add footer

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 function createGrid(sizeOfGrid){
-    const grid = document.querySelector('#grid');
+    const grid = document.createElement('div');
+    grid.setAttribute('id','grid');
+    grid.setAttribute('class','container')
     for(let i = 0; i < sizeOfGrid; i++){
         let row = document.createElement('div');
         row.setAttribute('class','container row');
         for(let j = 0; j < sizeOfGrid; j++){
             let column = document.createElement('div');
             column.classList.add('column')
-            column.textContent = 'C';
             row.appendChild(column);
         }
         grid.appendChild(row);   
     }
+    document.querySelector('#display').appendChild(grid);
 }
 
-createGrid(16);
+createGrid(32);
